c4b8f2ac4422523f3b18048783c274b01da1d614
hyj5076 <hyj5076@hanmail.net>
2023-10-20 22:00:52 +0900

    feat: 랜덤 수 생성 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class RandomNumberMaker {
+  constructor(randomNumber = '') {
+    this.randomNumber = randomNumber;
+  }
+
+  makeRandomNumber() {
+    while (this.randomNumber.length < 3) {
+      const newNumber = MissionUtils.Random.pickNumberInRange(1, 9).toString();
+      if (!this.randomNumber.includes(newNumber)) {
+        this.randomNumber += newNumber;
+      }
+    }
+    return this.randomNumber;
+  }
+}
+
+// Using the class
+const maker = new RandomNumberMaker();
+const uniqueNumber = maker.makeRandomNumber();
+console.log(uniqueNumber);
+
+/* 
 class App {
   async play() {}
 }
 
-export default App;
+export default App; */
+export default RandomNumberMaker;
